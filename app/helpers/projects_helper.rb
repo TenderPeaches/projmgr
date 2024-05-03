2634fd198ca381ab6290eb96a6eaa0042db2b8eb
Poubelle <tendrepeches@protonmail.com>
2024-05-03 13:40:10 -0400

    clean up project resource, application.html, stylesheets

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,4 +2,12 @@ module ProjectsHelper
     def project_list(projects)
         render("projects/table", projects: projects)
     end
+
+    def project_data(project)
+        render "shared/data", data: [
+            { label: "Hrs worked", value: project.hours_worked },
+            { label: "Hrs billed", value: project.hours_billed },
+            { label: "Pending pay", value: number_to_currency(project.pending_pay, precision: 2) },
+            { label: "Total pay", value: number_to_currency(project.total_pay, precision: 2) }, ]
+    end
 end
